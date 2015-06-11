089b558422ae670cd840f2b93dcaedc50ac518a4
Filipe Borges <filipe_bkc@yahoo.com.br>
2015-06-11 00:51:41 -0300

    Implemented DrawerLayout without ListView items

--- app/src/main/java/com/jabarasca/financial_app/MainActivity.java
+++ app/src/main/java/com/jabarasca/financial_app/MainActivity.java
@@ -1,25 +1,49 @@
 package com.jabarasca.financial_app;
 
-//import android.support.v7.app.ActionBarActivity;
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+//Obs: Normal actionBar from Activity doesnt show hamburguer icon.
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends Activity {
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DrawerLayout drawerLayout = (DrawerLayout)findViewById(R.id.activityMainDrawerLay);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+                R.string.open_drawer, R.string.close_drawer) {
+            @Override
+            public void onDrawerOpened (View drawerView) {}
+
+            @Override
+            public void onDrawerClosed (View drawerView) {}
+        };
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
     }
 
-    /*@Override
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        getMenuInflater().inflate(R.menu.activity_main_action_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        actionBarDrawerToggle.syncState();
     }
 
     @Override
@@ -27,13 +51,13 @@ public class MainActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event.
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
-    }*/
+    }
 }
