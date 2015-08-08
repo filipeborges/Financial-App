6e511a185ef206c8f2e37ca36789c66c4e842bac
Filipe Borges <filipe_bkc@yahoo.com.br>
2015-08-08 00:55:05 -0300

    Added Robotium and continuing to implement Instrumented Tests on MainActivity

--- app/src/androidTest/java/com/jabarasca/financial_app/test/FuncTestMainActivity.java
+++ app/src/androidTest/java/com/jabarasca/financial_app/test/FuncTestMainActivity.java
@@ -4,12 +4,15 @@ import junit.framework.Assert;
 
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v4.widget.DrawerLayout;
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.robotium.solo.Solo;
 
 import com.jabarasca.financial_app.MainActivity;
 import com.jabarasca.financial_app.R;
@@ -26,6 +29,7 @@ import org.junit.runner.RunWith;
 public class FuncTestMainActivity extends ActivityInstrumentationTestCase2<MainActivity>{
 
     private MainActivity activity;
+    private Solo solo;
 
     public FuncTestMainActivity() {
         super(MainActivity.class);
@@ -37,6 +41,28 @@ public class FuncTestMainActivity extends ActivityInstrumentationTestCase2<MainA
         //This was previously injected by the platforms old InstrumentationTestRunner and now need to be done manually.
         injectInstrumentation(InstrumentationRegistry.getInstrumentation());
         activity = getActivity();
+        solo = new Solo(getInstrumentation(), activity);
+    }
+
+    @Test
+    public void testAddIncome() {
+        solo.clickOnView(activity.findViewById(R.id.addButton));
+        DrawerLayout drawerLayout = (DrawerLayout)activity.findViewById(R.id.activityMainDrawerLay);
+        ListView rightDrawerListView = (ListView)activity.findViewById(R.id.activityMainRightDrawerListView);
+        getInstrumentation().waitForIdleSync();
+        Assert.assertTrue("Add menu is not openning.", drawerLayout.isDrawerOpen(rightDrawerListView));
+
+        solo.clickOnText(activity.getString(R.string.add_menu_option_2));
+        solo.enterText(0, "125.44");
+        solo.clickOnText(activity.getString(R.string.positive_button_message));
+        ViewGroup amountsListView = (ViewGroup)activity.findViewById(R.id.amountsListView);
+        getInstrumentation().waitForIdleSync();
+        Assert.assertNotNull("Income was not add to the amounts list.", amountsListView.getChildAt(0));
+
+        ImageView bottomBarGraphicImgView = (ImageView)activity.findViewById(R.id.bottomBarGraphicImgView);
+        Assert.assertTrue("Graphic should be: \"graphic_up\".",
+                bottomBarGraphicImgView.getDrawable().getConstantState().equals(activity.getResources().
+                        getDrawable(R.drawable.graphic_up).getConstantState()));
     }
 
     @Test
