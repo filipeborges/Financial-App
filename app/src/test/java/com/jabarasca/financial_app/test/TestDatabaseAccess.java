d0e1e4761a4afdd9766f04538bf72f987baeca81
Filipe Borges <filipe_bkc@yahoo.com.br>
2016-01-30 17:19:41 -0200

    Initiated DatabaseAccess unit test

---
+++ app/src/test/java/com/jabarasca/financial_app/test/TestDatabaseAccess.java
@@ -0,0 +1,20 @@
+package com.jabarasca.financial_app.test;
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestDatabaseAccess {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+}
