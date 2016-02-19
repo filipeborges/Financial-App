e0e55d12e5f034c478de2a4e041250c8fc32baa4
Filipe Borges <filipe_bkc@yahoo.com.br>
2016-02-19 00:40:01 -0200

    Changed CalendarView to DatePicker on select date layout

--- app/src/main/java/com/jabarasca/financial_app/CalendarActivity.java
+++ app/src/main/java/com/jabarasca/financial_app/CalendarActivity.java
@@ -1,14 +1,26 @@
 package com.jabarasca.financial_app;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.NumberPicker;
 
+//TODO: Make a method to calculate Max and Min Date of CalendarView;
+//TODO: Implement Ok Button callback.
 public class CalendarActivity extends Activity {
+
+    private DatePicker datePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        datePicker = (DatePicker)findViewById(R.id.datePicker);
+        datePicker.findViewById(Resources.getSystem().getIdentifier("day", "id", "android"))
+                .setVisibility(View.GONE);
     }
 
 }
